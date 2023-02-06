7cf20249d2acec97319f3bbcd6ff6b15aae60e55
王雪 <broken.ice1314@gmail.com>
2023-02-06 12:53:44 +0800

    fix: logo

--- pages/index.js
+++ pages/index.js
@@ -26,15 +26,15 @@ export default function Home() {
 
           <div>
             <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+              href="https://miantu.net?utm_source=zhengdaren&utm_medium=default-template&utm_campaign=zhengdaren"
               target="_blank"
               rel="noopener noreferrer"
             >
               By{" "}
               <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
+                src="/huanjing.svg"
+                alt="环璟科技 Logo"
+                className={styles.huanjingLogo}
                 width={100}
                 height={24}
                 priority
@@ -46,15 +46,15 @@ export default function Home() {
         <div className={styles.center}>
           <Image
             className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
+            src="/zhengdaren.svg"
+            alt="蒸达人 Logo"
             width={180}
             height={37}
             priority
           />
-          <div className={styles.thirteen}>
+          <div className={styles.hengshui}>
             <Image
-              src="/thirteen.svg"
+              src="/hengshui.svg"
               alt="13"
               width={40}
               height={31}
